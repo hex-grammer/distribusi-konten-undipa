aa842f415e99d60a0a25dddfc67fbdb0f432b290
Rizal Iswandy <=>
2022-12-06 02:21:58 +0800

    change / to \

--- pages/api/readfiles/index.ts
+++ pages/api/readfiles/index.ts
@@ -15,12 +15,12 @@ export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const dirRelativeToPublicFolder = `files`;
   // const dirRelativeToPublicFolder = `files/umum`;
 
-  const dir = path.resolve("./public", dirRelativeToPublicFolder);
+  const dir = path.resolve(".\\public", dirRelativeToPublicFolder);
 
   const filenames = fs.readdirSync(dir);
 
   const images = filenames.map((name) =>
-    path.join("/", dirRelativeToPublicFolder, name)
+    path.join("\\", dirRelativeToPublicFolder, name)
   );
 
   res.status(200).json({ images });
